13fbf340495097d0fbf418ecd1c22274cab87e16
Anatoliy Komarov <admin@komaroff.biz>
2021-07-13 15:38:20 +0300

    3

---
+++ components/left-menu.js
@@ -0,0 +1,18 @@
+document.querySelector(".new-inner-block").addEventListener("mouseover", () => {
+    document.querySelector(".new-inner-menu").classList.add("new-active-block")
+    document.querySelector(".new-after").src = `images/arrow-bottom-menu.png`
+})
+document.querySelector(".new-inner-block").addEventListener("mouseout", () => {
+    document.querySelector(".new-inner-menu").classList.remove("new-active-block")
+    document.querySelector(".new-after").src = `images/arrow-right-menu.png`
+})
+
+const listLinks = document.querySelectorAll(".new-leftMenu-color>li")
+for (let i = 0; i < listLinks.length; i++) {
+    listLinks[i].addEventListener("click", (event) => {
+        for (let z = 0; z < listLinks.length; z++) {
+            listLinks[z].classList.remove("new-active")
+        }
+        event.target.parentNode.classList.add("new-active")
+    })
+}
