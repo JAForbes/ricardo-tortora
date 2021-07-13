7782214fa5bb9adcd5aedf800a704b44cdae0972
Anatoliy Komarov <admin@komaroff.biz>
2021-07-13 17:27:05 +0300

    1

--- components/left-menu.js
+++ components/left-menu.js
@@ -7,11 +7,11 @@ document.querySelector(".new-inner-block").addEventListener("mouseout", () => {
     document.querySelector(".new-after").src = `images/arrow-right-menu.png`
 })
 
-const listLinks = document.querySelectorAll(".new-leftMenu-color>li")
+const listLinks = document.querySelectorAll(".new-leftMenu-color>li>a")
 for (let i = 0; i < listLinks.length; i++) {
     listLinks[i].addEventListener("click", (event) => {
         for (let z = 0; z < listLinks.length; z++) {
-            listLinks[z].classList.remove("new-active")
+            listLinks[z].parentNode.classList.remove("new-active")
         }
         event.target.parentNode.classList.add("new-active")
     })
