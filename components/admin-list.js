01dede095d96e19a61dff6749c880858ac584a9b
Anatoliy Komarov <admin@komaroff.biz>
2021-07-20 11:16:51 +0300

    2

--- components/admin-list.js
+++ components/admin-list.js
@@ -9,10 +9,13 @@ for (let i = 0; i < els.length; i++){
     })
 }
 
+///
+//
 // const els = document.querySelectorAll(".block-close")
 // const blocks = document.querySelectorAll(".hidden.grid.grid-cols-1.w-full")
-// let listFlag = true
+// // let listFlag = true
 // for (let i = 0; i < els.length; i++){
+//     let listFlag = true
 //     els[i].addEventListener("click",(event) =>{
 //         if(listFlag) {
 //             for (let z = 0; z < blocks.length; z++) {
@@ -24,7 +27,39 @@ for (let i = 0; i < els.length; i++){
 //             for (let z = 0; z < blocks.length; z++) {
 //                 blocks[z].classList.add("hidden")
 //             }
+//             //els[i].querySelector(".hidden.grid.grid-cols-1.w-full").classList.remove("hidden")
 //             listFlag = true
 //         }
+//     })
+// }
+
+
+//
+// const els = document.querySelectorAll(".block-close")
+// const blocks = document.querySelectorAll(".hidden.grid.grid-cols-1.w-full")
+// let listFlag = true
+// for (let i = 0; i < els.length; i++){
+//     //let listFlag = true
+//     els[i].addEventListener("click",(event) =>{
+//         if(listFlag) {
+//             for (let z = 0; z < blocks.length; z++) {
+//                 blocks[z].classList.add("hidden")
+//             }
+//             els[i].querySelector(".hidden.grid.grid-cols-1.w-full").classList.remove("hidden")
+//             listFlag = false
+//         } else {
+//             for (let z = 0; z < blocks.length; z++) {
+//                 blocks[z].classList.add("hidden")
+//             }
+//             //els[i].querySelector(".hidden.grid.grid-cols-1.w-full").classList.remove("hidden")
+//             listFlag = true
+//         }
+//         searchTrue = document.querySelectorAll(".hidden.grid.grid-cols-1.w-full")
+//         console.log(" all blocks = "+ blocks.length + " | now = "+ searchTrue.length)
+//
+//         //  if(blocks.length === searchTrue.length){
+//         //      els[i].querySelector(".hidden.grid.grid-cols-1.w-full").classList.remove("hidden")
+//         // listFlag = false
+//         // }
 //     })
 // }
